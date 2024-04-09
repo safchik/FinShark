ab62223f959b73522f07525b13807b0d60ab5a38
safchik <safchik9@gmail.com>
2024-04-09 17:06:47 +0100

    data flow corrected

--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,14 +1,12 @@
-import React, { ChangeEvent, useState, MouseEvent, SyntheticEvent } from 'react'
+import React, { ChangeEvent, useState, MouseEvent, SyntheticEvent, FormEvent } from 'react'
 
-type Props = {};
+interface Props {
+    onClick: (e: SyntheticEvent) => void;
+    search: string | undefined;
+    handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+};
 
-const Search: React.FC<Props> = (props: Props): JSX.Element => {
-    const [search, setSearch] = useState<string>("");
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setSearch(e.target.value);
-        console.log(e);
-    };
-    const onClick = (e: SyntheticEvent) => { console.log(e) };
+const Search: React.FC<Props> = ({ onClick, search, handleChange }: Props): JSX.Element => {
 
     return (
         <div>
