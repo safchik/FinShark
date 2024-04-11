be73933d26c1d5cd74262e66c639e32c7eb7c9ad
safchik <safchik9@gmail.com>
2024-04-11 19:26:38 +0100

    use effect added for a company page

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { CompanyProfile } from '../../company';
+import { getCompanyProfile } from "../../api";
+import { useParams } from "react-router-dom";
 
 interface Props { }
 
 const CompanyPage = (props: Props) => {
+    let { ticker } = useParams();
+    const [company, setCompany] = useState<CompanyProfile>();
+
+    useEffect(() => {
+        const getProfileInit = async () => {
+            const result = await getCompanyProfile(ticker!);
+            setCompany(result?.data[0]);
+        }
+        getProfileInit();
+    }, []);
     return (
-        <div>CompanyPage</div>
+        <>
+            {company ? (
+                <div>{company.companyName}</div>
+            ) : (
+                <div>Company not found!</div>
+            )}
+        </>
     )
 }
 
