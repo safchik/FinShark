a0072231251f27f56a8a45a463d278c6fc2a07b9
safchik <safchik9@gmail.com>
2024-04-15 15:57:05 +0100

    errors fixed, detail page finished

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -23,7 +23,7 @@ export const searchCompanies = async (query: string) => {
 export const getCompanyProfile = async (query: string) => {
     try {
         const data = await axios.get<CompanyProfile[]>(
-            `https://financialmodelingprep.com/api/v3/search?query=${query}?apikey=${process.env.REACT_APP_API_KEY}`
+            `https://financialmodelingprep.com/api/v3/profile/${query}?apikey=${process.env.REACT_APP_API_KEY}`
         )
         return data;
     } catch (error: any) {
