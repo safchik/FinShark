bd47172238b49b525360f093426488b7fe41fb01
safchik <safchik9@gmail.com>
2024-04-16 10:39:12 +0100

    income table component with mock data implemented

---
+++ frontend/src/Pages/DesignPage/DesignPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Table from '../../Components/Table/Table'
+
+type Props = {}
+
+const DesignPage = (props: Props) => {
+    return (
+        <div>
+            <h1>FinShark Design Page</h1>
+            <Table />
+        </div>
+    )
+}
+
+export default DesignPage
