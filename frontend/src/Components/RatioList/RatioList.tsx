37335c19da7031340a484e00601da90064a13ad7
safchik <safchik9@gmail.com>
2024-04-16 11:28:49 +0100

    ratio list component with some styling implemented

---
+++ frontend/src/Components/RatioList/RatioList.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { TestDataCompany } from '../Table/testData'
+
+type Props = {}
+
+const data = TestDataCompany[0];
+
+type Company = typeof data;
+
+const config = [
+    {
+        label: "Company Name",
+        render: (company: Company) => company.companyName,
+        subTitle: "This is company",
+    },
+    {
+        label: "Company Name",
+        render: (company: Company) => company.companyName,
+        subTitle: "This is company",
+    }
+]
+
+const RatioList = (props: Props) => {
+    const renderedRows = config.map(row => {
+        return (
+            <li className='py-3 sm:py-4'>
+                <div className='flex items-center space-x-4'>
+                    <div className="flex-1 min-w-0">
+                        <p className='text-sm font-medium text-gray-900 truncate'>{row.label}</p>
+                        <p className="text-sm text-gray-500 truncate">{row.subTitle && row.subTitle}</p>
+                    </div>
+
+                    <div className="inline-flex items-center text-base font-semibold text-gray-900">
+                        {row.render(data)}
+                    </div>
+                </div>
+            </li>
+        )
+    })
+    return (
+        <div className='bg-white shadow rounded-lg ml-4 mt-4 mb-4 p-4 sm:p-6 w-full'>
+            <ul className='divide-y divide-gray-200'>{renderedRows}</ul>
+        </div>
+    )
+}
+
+export default RatioList
