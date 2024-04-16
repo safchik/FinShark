2a74cc139a2bbafa9d64d10776cd51a399f8ced2
safchik <safchik9@gmail.com>
2024-04-16 12:28:59 +0100

    company profile with real data revenues and earnings company receives

--- frontend/src/Pages/DesignPage/DesignPage.tsx
+++ frontend/src/Pages/DesignPage/DesignPage.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import Table from '../../Components/Table/Table'
 import RatioList from '../../Components/RatioList/RatioList'
+import { testIncomeStatementData } from '../../Components/Table/testData';
 
 type Props = {}
+
+const tableConfig = [
+    {
+        label: "Market Cap",
+        render: (company: any) => company.marketCapTTM,
+        subTitle: "Total value of all a company's shares of stock",
+    },
+];
 
 const DesignPage = (props: Props) => {
     return (
@@ -11,7 +20,7 @@ const DesignPage = (props: Props) => {
                 Design guide- This is the design guide for Fin Shark. These are reuable
                 components of the app with brief instructions on how to use them.
             </h1>
-            <RatioList />
+            <RatioList data={testIncomeStatementData} config={tableConfig} />
             <Table />
             <h3>
                 Table - Table takes in a configuration object and company data as
