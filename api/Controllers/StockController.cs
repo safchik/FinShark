fb6071f3b19a42b28eb7ec47f407ad09745bfa3d
Dmytro <sagchik9@gmial.com>
2024-04-25 16:27:33 +0100

    preparing controllers

---
+++ api/Controllers/StockController.cs
@@ -0,0 +1,17 @@
+﻿using api.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+        public StockController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+    }
+}
