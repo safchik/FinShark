641d749ed88dfb1aace63f67fc1484abf999b91e
Dmytro <sagchik9@gmial.com>
2024-04-25 17:33:49 +0100

    Stock dto, stock mapper added

---
+++ api/Mappers/StockMapper.cs
@@ -0,0 +1,22 @@
+﻿using api.Dtos.Stock;
+using api.Model;
+
+namespace api.Mappers
+{
+    public static class StockMapper
+    {
+        public static StockDto ToStockDto(this Stock stockModel)
+        {
+            return new StockDto
+            {
+                Id = stockModel.Id,
+                Symbol = stockModel.Symbol,
+                CompanyName = stockModel.CompanyName,
+                Purchase = stockModel.Purchase,
+                LastDiv = stockModel.LastDiv,
+                Industry = stockModel.Industry,
+                MarketCap = stockModel.MarketCap,
+            };
+        }
+    }
+}
