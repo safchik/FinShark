72af29988e37ec733b356caef595d4719828fc49
Dmytro <sagchik9@gmial.com>
2024-04-30 16:31:36 +0100

    comment controller, mapper dto, repository and interface implemented

---
+++ api/Mappers/CommentMapper.cs
@@ -0,0 +1,21 @@
+﻿using api.Dtos.Comment;
+using api.Model;
+using System.Runtime.CompilerServices;
+
+namespace api.Mappers
+{
+    public static class CommentMapper
+    {
+        public static CommentDto ToCommentDto(this Comment commentModel)
+        {
+            return new CommentDto
+            {
+                Id = commentModel.Id,
+                Title = commentModel.Title,
+                Content = commentModel.Content,
+                CreatedOn = commentModel.CreatedOn,
+                StockId = commentModel.StockId,
+            };
+        }
+    }
+}
